d8fe6a7acb7f789306b256e7bf36ff46907db3dd
printMelen <alvaro.redondofgb@gmail.com>
2025-03-08 17:53:29 +0100

    Añadiendo la conexion a la base de datos

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 use Printmelen\NftPortalApi\controllers\UserController;
+use Printmelen\NftPortalApi\db\Database;
 header('Content-Type: application/json; charset=utf-8');
 $url=explode('/', $_SERVER['REQUEST_URI']);
 if ($url[2] != 'user') {
@@ -9,6 +10,8 @@ if ($url[2] != 'user') {
 }
 
 $id = $url[3] ?? null;
+$database= new Database('localhost', 'nftportal', 'root', ''); // Database connection create a config file
+$database->getConnection();
 $controller = new UserController();
 $controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
 echo $id;
